515440971016a43391673fd012a3d494bae4b6a3
StartXia <1375803993@qq.com>
2023-11-21 11:10:08 +0800

    修改了HelloWorld

--- module/module-web/src/main/java/com/monster/module/webapp/controller/HelloWorldController.java
+++ module/module-web/src/main/java/com/monster/module/webapp/controller/HelloWorldController.java
@@ -6,5 +6,8 @@ package com.monster.module.webapp.controller;
  * @version $Id: HelloWorldController.java, v 0.1 2019年3月7日 下午3:08:38 xia Exp $
  */
 public class HelloWorldController {
-
+    
+    public static void main(String[] args) {
+        System.out.println("有些东西");
+    }
 }
